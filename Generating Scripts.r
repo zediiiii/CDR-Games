fe674bc5d9580dee1743ca6cf31c0747f92da1dc
zediiiii <zediiiii@gmail.com>
2015-11-13 13:26:49 -0700

    Re-added the permutation sorting script

--- Generating Scripts.r
+++ Generating Scripts.r
@@ -119,3 +119,15 @@ cdrindex <- function(gamestate){
     list<-gen.cdrpile(n)
     which(sapply(list, identical, gamestate ))
 }
+
+####################
+# function:     sort.listss()
+# purpose:    	Sorts a list of lists in ascending order.
+# parameters:	x:A list of lists. This function
+# Author:       Joshua Watson Oct 2015
+
+sort.listss <- function(list){
+    list->list.of.lists    
+    list.of.lists[order(sapply(list.of.lists,'[[',1))]
+    
+}
