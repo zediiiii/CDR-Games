d05aecc4bdf4ff1c44d9c2edace385a2c4a3bda2
zediiiii <zediiiii@gmail.com>
2015-11-18 20:26:17 -0700

    Progress

--- cdrmap3.R
+++ cdrmap3.R
@@ -3,14 +3,53 @@
 #convert a back from character   string scan(text = a, what = 0L, sep=" ")
 #another way                     as.integer(unlist(strsplit(a," ")))
 
-make.tree <- function(gamestate){
+#for(j in cdrpointers(x[[3]])){ass(cdrmove(gamestate,j))}
+
+data.frame()->df
+i=0
+ass<- function(child,parent){
+    child<-paste(child,collapse=" ")
+    parent<-paste(parent,collapse=" ")
+    nam <- paste("v", i, sep = "")
+    assign(nam, paste0(child))
+    
+    assign(nam,append(parent,child))->a
+    a
+    #i+1
+    #df$nam <-a
+}
+
+
+i=0
+ass<- function(child,parent,varname){
+    child<-paste(child,collapse=" ")
+    parent<-paste(parent,collapse=" ")
+    varname<-append(parent,child)    
+}
+
+    make.tree <- function(gamestate){
     
     #generate a list of vector names that will be longer that the depth of the tree for assignment v1,v2,vn
-    my_keys <- letters[1:26]
-    mylist <- vector(mode="list", length=length(my_keys))
-    names(mylist) <- my_keys
+    my.keys <- paste("v",1:1000,sep="")
+    keylist <- vector(mode="list", length=length(my.keys))
+    names(keylist) <- my.keys
+    my.keys[1]
+    
+    
+    
+    
+    }
+    
+    for(j in cdrpointers(gamestate)){
+        ass(cdrmove(gamestate,j))
+    }
+    
+    
+    ass(i,gamestate)
     
     v1<-gamestate
+
+    
     
     #Generate the first set of children
     #note, generating the new vector outside the loops will optimize code use
