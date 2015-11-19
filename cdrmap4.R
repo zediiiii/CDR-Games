ef37e6058412abc1b2c8451c34ff49ceb18eacaa
zediiiii <zediiiii@gmail.com>
2015-11-19 03:23:45 -0700

    Still stuck, but this one has the best shot so far.

--- cdrmap4.R
+++ cdrmap4.R
@@ -1,6 +1,4 @@
-
-
-#v0 <- Node$new(paste(x[[2]],collapse=" "))
+require(data.tree)
 
 # Helper function to insert nodes as child of parent
 i=1
@@ -13,25 +11,21 @@ assn <- function(child,parentvarname){
     a                   #output the child variable name, ie, vn, for the sake of recursion
 }
 
-
-require(data.tree)
 cdrtree<<- function(root){
 
-    
     #assign root
     v0 <<- Node$new(root)
     node<-root
     kidparentname<-v0
         have.kids<-function(node){   #this is unfortunately asexual reproduction...
                 for(pointer in cdrpointers(node)){
-                cdrmove(node,pointer)->newkid
-                assn(newkid,kidparentname) #enter this node in the tree hierarchy
-                kidparentname<-assn(newkid,kidparentname)   #get the name of the newkid for the next iteration
-                node<-newkid    
+                    cdrmove(node,pointer)->newkid
+                    assn(newkid,kidparentname) #enter this node in the tree hierarchy
+                    kidparentname<-assn(newkid,kidparentname)   #get the name of newkid for the next iteration
+                    node<-newkid    
                     have.kids(newkid)   #recurse
-                    
-            }
-    #find kids
+                }
+    #return the tree
     return(v0)
-}
+        }
 }
