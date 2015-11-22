103bdc16bbba54258ee6f71ab1fe5803843deb43
zediiiii <zediiiii@gmail.com>
2015-11-21 22:21:43 -0700

    Bugs squashed, format as pretty as possible. Looks good.

--- cdrmap5.R
+++ cdrmap5.R
@@ -15,21 +15,24 @@ nodeNamer <- function() {
 }
 
 nodeNamer2 <- function() {
-  j <- 0
-  function(node) sprintf("%g", (j <<- j+1))
+    j <- 0
+    function(node) sprintf("%g", (j <<- j+1))
 }
 
-cdrtree <- function(root.value, make.igraph=FALSE) {
+cdrtree <- function(root.value, shorthand=FALSE,ouputastext=TRUE) {
     
     templist<- list()
     thispile<-gen.cdrpile(length(root.value))
     root <- Node$new('v0')  
     root$value <- root.value  
-    root$name <- paste("ROOT",paste(unlist(root$value),collapse=' ')) #name this the same as the value collapsed in type char
+    if(shorthand==FALSE){
+        root$name <- paste("ROOT",paste(unlist(root$value),collapse=' ')) #name this the same as the value collapsed in type char
+    } else{
+        root$name <-paste("R^",length(root.value),"_",cdrindex(root.value),collapse=",")
+    }
     
     name.node <- nodeNamer()   # initialize the node counters to name the nodes
     name.node2 <- nodeNamer2()
-    
     #recursive function that produces children and names them appropriately
     have.kids <- function(node) {
         pointers <- tryCatch({cdrpointers(node$value)}, error=function(e) return( list() ))
@@ -37,45 +40,60 @@ cdrtree <- function(root.value, make.igraph=FALSE) {
         for (pointer in pointers) {
             
             child.val <- cdrmove(node$value, pointer)  #make the cdr move on the first pointer
-            
+            child.index<-cdrindex(child.val)
             child <- Node$new(name.node())
             child$value <- child.val
-
-            child$name <- paste(unlist(child$value),collapse=' ')  # Name it 
+            
+            if(shorthand==TRUE){
+                prefix<-name.node2()
+                namevar<-paste("R_",child.index,sep="")
+                child$name<-namevar
+            }
+            if(shorthand==FALSE){
+                child$name <- paste(unlist(child$value),collapse=' ')  # Name it 
+                namevar<-child$name
+                prefix<-name.node2()
+                
+            }
+            
             child <- node$AddChildNode(child)
             
             #identical ending name handling catches duplicates. Names WIN+, WIN-, and DRAW outcomes
             endname<-paste(unlist(tail(thispile, n=1)[[1]]),collapse=' ')
             startname<-paste(unlist(thispile[[1]]),collapse=' ')
             
+            
+            
             if(child$name==endname){
-                child$name <- paste(name.node2(),"-WIN ",child$name,sep='')  
+                child$name <- paste(prefix,"-W ",namevar,sep='')  
             } else {
-                    if(child$name==startname){
-                        child$name <- paste(name.node2(),"+WIN ",child$name,sep='')  
+                if(child$name==startname){
+                    child$name <- paste(prefix,"+W ",namevar,sep='')  
+                } else {
+                    #if all negative (!win) or all positive (!win) then it is terminal and could be a duplicate, rename it for igraph
+                    if((sum(child$value < 0) == length(root.value)) || ((sum(child$value < 0 ) == 0 && !(child$name==endname) ) )){
+                        child$name <- paste(prefix,"S ",namevar,sep='')
                     } else {
-                        #if all negative (!win) or all positive (!win) then it is terminal and could be a duplicate, rename it for igraph
-                            if((sum(child$value < 0) == length(root.value)) || ((sum(child$value < 0 ) == 0 && !(child$name==endname) ) )){
-                                child$name <- paste(name.node2(),"DRAW ",child$name,sep='')
-                            } else {
-                                #catch the other duplicate cases that aren't listed above
-                                if((child$name %in% templist == TRUE) || (child$name == root$name)){
-                                    child$name <- paste(name.node2(),"DUP ",child$name,sep='')
-                                    #templist[[length(pointerlist)+1]] <-
-                                } 
-                            }
+                        #catch the other duplicate cases that aren't listed above
+                        if((child$name %in% templist == TRUE) || (child$name == root$name)){
+                            child$name <- paste(prefix,"D ",namevar,sep='')
+                        } 
                     }
+                }
                 
             }
             #make a list of names for the last duplicate catcher
             append(child$name,templist)->>templist
-            child$name <- paste(" ",child$name,collapse=' ') #add a space for cosm
+            
+            if(shorthand==FALSE){
+                child$name <- paste(" ",child$name,collapse=' ') #add a space for cosmetics
+            }
             
             Recall(child)    # recurse with child
-            }
         }
+    }
     have.kids(root)
-    return( root )
+        return( root )
 }
 
 #to plot
@@ -96,11 +114,11 @@ cdrtextforrest <- function(pile){
         
         filename<-paste(cdrindex(i),"    ",paste(i,collapse=' '),".txt",sep="")
         
-            write.table(cdrtree(i), filename,quote = FALSE,col.names = FALSE, row.names = FALSE)
+        write.table(cdrtree(i), filename,quote = FALSE,col.names = FALSE, row.names = FALSE)
     }
     setwd(wd)       
 }
-      
+
 #Doesn't work yet somethin to do with line 119
 treegraph<-function(tree){
     
@@ -141,4 +159,3 @@ imagebiome<- function(range){
     setwd(wd)
 }
 
-    
